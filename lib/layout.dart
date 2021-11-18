d6f9f16336c1cbc6247725143e400d7dc4a2ca4d
Frank David Addae <frankdavid.addae@gmail.com>
2021-11-18 13:11:42 +0000

    responsive layout

--- lib/layout.dart
+++ lib/layout.dart
@@ -2,6 +2,8 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_web_dashboard/widgets/large_screen.dart';
+import 'package:flutter_web_dashboard/widgets/responsive_widget.dart';
+import 'package:flutter_web_dashboard/widgets/small_screen.dart';
 
 class SiteLayout extends StatelessWidget {
   const SiteLayout({Key? key}) : super(key: key);
@@ -13,7 +15,10 @@ class SiteLayout extends StatelessWidget {
         elevation: 0.0,
         backgroundColor: Colors.white,
       ),
-      body: LargeScreen(),
+      body: ResponsiveWidget(
+        largeScreen: LargeScreen(),
+        smallScreen: SmallScreen(),
+      ),
     );
   }
 }
